35d998f74ada6d03c8846cd71e9c5f53f234fe86
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2024-03-09 20:31:40 -0700

    Create mayhem_worm_mutator.h

---
+++ mayhem_worm_mutator.h
@@ -0,0 +1,29 @@
+#ifndef MAYHEM_WORM_MUTATOR_H
+#define MAYHEM_WORM_MUTATOR_H
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <ctime>
+#include <filesystem>
+
+struct ASTNode {
+    std::string type;
+    std::vector<ASTNode*> children;
+    ASTNode(std::string t, std::vector<ASTNode*> c) : type(t), children(c) {}
+
+    std::string getAttribute(std::string attributeName);
+};
+
+class MayhemWorm {
+public:
+    void deleteAST(ASTNode* root);
+    void applyCodeTemplates(ASTNode* root);
+    void mutateCode();
+    std::string generateMutatedCode();
+};
+
+#endif // MAYHEM_WORM_MUTATOR_H
