233644ddeb5271e362733a5b12e16161be0dfd6b
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2024-03-09 20:32:08 -0700

    Create enigmaticWorm.h

---
+++ enigmaticWorm.h
@@ -0,0 +1,20 @@
+#ifndef ENIGMATIC_WORM_H
+#define ENIGMATIC_WORM_H
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <random>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+class EnigmaticWorm {
+public:
+    void crypticControlFlow();
+    void vanishConsole();
+    void obscuredEntryPoint();
+};
+
+#endif // ENIGMATIC_WORM_H
